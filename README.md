2fa8cd5b219fe1e7d3fddad2cfc6fb4b84e8ca6b
djshaw <21282692+djshaw@users.noreply.github.com>
2024-01-23 04:18:08 +0000

    Figured out how to convert an aribtrary wav to a wav that asterisk supports. Starting to work on 2 extensions, instead of just one.  Documenting the settings that the Linksys PAP2 needs

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # toddlers-first-asterisk
-A simple asterisk configuration with a single digit dialplans for a toddler to learn how to use an analogue phone
+A simple asterisk configuration with a single digit dialplans for a toddler to
+learn how to use an analogue phone
 
 Build with:
 ```
@@ -10,4 +11,28 @@ The build script starts asterisk.
 
 I was unable to get Asterisk to work in a docker container, but it is straight
 forward to get it running in an lxd container.
+
+
+# PAP2 Configuration
+
+To configure a Linksys PAP2 to connect to Asterisk:
+
+| Configuration Page | Key       | Value              | Source of value in Asterisk |
+| -                  | -         | -                  | -                           |
+| Line 1             | SIP Port  | 5060               | (Default value)             |
+|                    | User ID   | 6001               | `/etc/asterisk/pjsip.conf`: Group name and `6001.username=6001 |
+|                    | Password  | `unsecurepassword` | `/etc/asterisk/pjsip.conf`: `6001.password=unsecurepassword` |
+|                    | Dial Plan | `x`                | Implicitly required based on the content of `/etc/asterisk/extensions.conf` |
+
+
+# Debugging Asterisk
+
+Connect to the Asterisk console with
+```
+sudo asterisk -r
+```
+
+
+# `.wav` Files
+
 
--- build.sh
+++ build.sh
@@ -5,61 +5,67 @@ set -x
 set -o pipefail
 set -u
 
-# TODO: specify tag for ubuntu
-# TODO: asterisk version as a build arg
+if [ "$EUID" -ne 0 ] ; then
+    # This script needs to run as root so it can install binaries
+    sudo $0
+    exit $?
+fi
 
-# TODO: put asterisk tar source file in a cache
 # TODO: probably don't need libncurses5-dev--I presume it's only used in `make menuselect`
 export DEBIAN_FRONTEND=noninteractive
 
-apt update
-apt install --assume-yes \
-            build-essential \
-            curl \
-            debconf-utils \
-            git \
-            lame \
-            libjansson-dev \
-            libncurses5-dev \
-            libnewt-dev \
-            libsqlite3-dev \
-            libssl-dev \
-            libxml2-dev  \
-            subversion \
-            tzdata \
-            uuid-dev \
-            wget
-if [ ! -f /tmp/asterisk-21.0.1.tar.gz ] ; then
-    wget https://downloads.asterisk.org/pub/telephony/asterisk/old-releases/asterisk-21.0.1.tar.gz \
-         --output-document=/tmp/asterisk-21.0.1.tar.gz
-fi
+ASTERISK_VERSION=21.0.1
+if [[ ! $( asterisk -V ) =~ ${ASTERISK_VERSION}$ ]] ; then
+    apt update
+    apt install --assume-yes \
+                build-essential \
+                curl \
+                debconf-utils \
+                git \
+                lame \
+                libjansson-dev \
+                libncurses5-dev \
+                libnewt-dev \
+                libsqlite3-dev \
+                libssl-dev \
+                libxml2-dev  \
+                subversion \
+                tzdata \
+                uuid-dev \
+                wget
 
-pushd /tmp
-    # TODO: it wouldn't be unreasonable to untar and build in a temporary
-    # directory
-    # Only unpack if we've not unpacked before. Otherwise, any subsequent
-    # builds will rebuild everything
-    if [[ ! -d asterisk-21.0.1 ]] ; then
-        tar xvf asterisk-21.0.1.tar.gz
+    if [ ! -f /tmp/asterisk-$ASTERISK_VERSION.tar.gz ] ; then
+        wget https://downloads.asterisk.org/pub/telephony/asterisk/old-releases/asterisk-$ASTERISK_VERSION.tar.gz \
+             --output-document=/tmp/asterisk-$ASTERISK_VERSION.tar.gz
     fi
-    pushd asterisk-*/
-        # If the mp3 source is already downloaded, get_mp3_source.sh will
-        # return a non-0 value
-        contrib/scripts/get_mp3_source.sh || true
-        # In `install_prereq install`, libvpb1 waits for you to enter your local
-        # country code
-        echo 'libvpb1 libvpb1/countrycode     string  1' | debconf-set-selections -v
 
-        # install_prereq fails if it was already run
-        contrib/scripts/install_prereq install || true
-        ./configure
-        make -j $( nproc )
-        make install
-        make samples
-        make config
-        ldconfig
+    pushd /tmp
+        # TODO: it wouldn't be unreasonable to untar and build in a temporary
+        # directory
+        # Only unpack if we've not unpacked before. Otherwise, any subsequent
+        # builds will rebuild everything
+        if [[ ! -d asterisk-$ASTERISK_VERSION ]] ; then
+            tar xvf asterisk-$ASTERISK_VERSION.tar.gz
+        fi
+        pushd asterisk-*/
+            # If the mp3 source is already downloaded, get_mp3_source.sh will
+            # return a non-0 value
+            contrib/scripts/get_mp3_source.sh || true
+            # In `install_prereq install`, libvpb1 waits for you to enter your local
+            # country code
+            echo 'libvpb1 libvpb1/countrycode     string  1' | debconf-set-selections -v
+
+            # install_prereq fails if it was already run
+            contrib/scripts/install_prereq install || true
+            ./configure
+            make -j $( nproc )
+            make install
+            make samples
+            make config
+            ldconfig
+        popd
     popd
-popd
+fi
 
 cp etc/asterisk/* /etc/asterisk
 
@@ -67,7 +73,7 @@ cp etc/asterisk/* /etc/asterisk
 groupadd asterisk || true
 useradd -r -d /var/lib/asterisk -g asterisk asterisk || true
 usermod -aG audio,dialout asterisk
-chown -R asterisk.asterisk /etc/asterisk \
+chown -R asterisk:asterisk /etc/asterisk \
                            /var/{lib,log,spool}/asterisk \
                            /usr/lib/asterisk
 
@@ -91,13 +97,29 @@ fi
 
 # I don't want to host the mp3 files because I don't know I'm allowed to host
 # them. Get the source files from a place that only I can access.
-scp -B cerf@djshaw.ca:sounds/* sounds || true
+scp -B cerf@git.djshaw.ca:sounds/* sounds || true
+
+# TODO: convert the files to 16bit endian, 8000khz
+# sox chase-s01e02a.wav.old -b 16 -c 1 -r 8000 chase
+
+# You might not be able to download the clips from my server.  In that case,
+# continue on with the installation.
+set +x
+rm -rf /var/lib/asterisk/sounds/en/{.*,*}
+set -x
+cp sounds/*.{wav,gsm}* /var/lib/asterisk/sounds/en/ || true
+
+# TODO: this could be an -exec or an xargs
 for FILE in $( ls sounds/*.mp3 ) ; do
     # TODO: put the output file name in a variable
     lame --decode $FILE /var/lib/asterisk/sounds/en/$( basename $FILE ).wav
-    chown asterisk:asterisk /var/lib/asterisk/sounds/en/$( basename $FILE ).wav
-    chmod 644 /var/lib/asterisk/sounds/en/$( basename $FILE ).wav
 done
+
+# There are too many files for the -x output to be useful
+set +x
+chown asterisk:asterisk /var/lib/asterisk/sounds/en/*
+chmod 644 /var/lib/asterisk/sounds/en/*
+set -x
 
 systemctl restart asterisk
 systemctl enable asterisk
